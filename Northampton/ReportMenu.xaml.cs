a7e1de159868159117b1d17ee76df46d922a8161
Kevin White <kwhite@northampton.gov.uk>
2019-04-20 16:00:01 +0100

    WebService handler written

--- Northampton/ReportMenu.xaml.cs
+++ Northampton/ReportMenu.xaml.cs
@@ -11,12 +11,15 @@ namespace Northampton
         public ReportMenu()
         {
             InitializeComponent();
-
         }
 
         async void AtThisLocationButtonClicked(object sender, EventArgs args)
         {
-            await Navigation.PushAsync(new ReportCurrentLocationPage());
+            //await Navigation.PushAsync(new ReportCurrentLocationPage());
+            Application.Current.Properties["WebServiceHandlerPageTitle"] = "Report a problem";
+            Application.Current.Properties["WebServiceHandlerPageDescription"] = "Report a problem description";
+            await Application.Current.SavePropertiesAsync();
+            await Navigation.PushAsync(new WebServiceHandlerPage("ReportMenu"));
         }
 
     }
---
+++ Northampton/WebServiceHandlerPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Northampton
+{
+    public partial class WebServiceHandlerPage : ContentPage
+    {
+
+        private string pageTitle = "";
+        private string pageDescription = "";
+
+        public WebServiceHandlerPage(String callingPage)
+        {
+            InitializeComponent();
+            if (Application.Current.Properties.ContainsKey("WebServiceHandlerPageTitle"))
+            {
+                pageTitle = Application.Current.Properties["WebServiceHandlerPageTitle"] as String;
+            }
+            if (Application.Current.Properties.ContainsKey("WebServiceHandlerPageDescription"))
+            {
+                pageDescription = Application.Current.Properties["WebServiceHandlerPageDescription"] as String;
+            }
+            BindingContext = this;
+            switch (callingPage)
+            {
+                case "ReportMenu":
+                    GetCurrentLocation();
+                    break;
+                default:
+                    Navigation.PopAsync();
+                    break;
+            }
+        }
+
+        public string PageTitle
+        {
+            get
+            {
+                return pageTitle;
+            }
+        }
+
+        public string PageDescription
+        {
+            get
+            {
+                return pageDescription;
+            }
+        }
+
+        async void GetCurrentLocation()
+        {
+            Console.WriteLine("Checking for location...");
+            Location currentLocation = null;
+            try
+            {
+                var locationRequest = new GeolocationRequest(GeolocationAccuracy.Best);
+                currentLocation = await Geolocation.GetLocationAsync(locationRequest);
+
+                if (currentLocation != null)
+                {
+                    Console.WriteLine($"Latitude: {currentLocation.Latitude}, Longitude: {currentLocation.Longitude}, Altitude: {currentLocation.Altitude}");
+                    Application.Current.Properties["Latitude"] = currentLocation.Latitude;
+                    Application.Current.Properties["Longtitude"] = currentLocation.Longitude;
+                    await Application.Current.SavePropertiesAsync();
+                }
+
+            }
+            catch (FeatureNotSupportedException error)
+            {
+                Console.WriteLine("Error1 - " + error);
+            }
+            catch (FeatureNotEnabledException error)
+            {
+                Console.WriteLine("Error2 - " + error);
+            }
+            catch (PermissionException error)
+            {
+                Console.WriteLine("Error3 - " + error);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error4 - " + error);
+            }
+            Console.WriteLine("returning");
+
+
+            WebRequest streetRequest = WebRequest.Create(string.Format(@"https://veolia-test.northampton.digital/api/GetStreetByLatLng?lat={0}&lng={1}", currentLocation.Latitude, currentLocation.Longitude));
+            streetRequest.ContentType = "application/json";
+            streetRequest.Method = "GET";
+
+            using (HttpWebResponse response = streetRequest.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    var content = reader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(content))
+                    {
+                        Console.Out.WriteLine("Response contained empty body...");
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("Response Body: \r\n {0}", content);
+                        Application.Current.Properties["JsonStreet"] = content;
+                        await Application.Current.SavePropertiesAsync();
+                    }
+                }
+            }
+
+            await Navigation.PushAsync(new ReportDetailsPage());
+        }
+    }
+}
